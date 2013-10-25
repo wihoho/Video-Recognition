99f65c89355fe4538888b4440ea6117817b07aa2
wihoho <wihoho@gmail.com>
2013-10-25 14:28:03 +0800

    add confusion figures

--- test.py
+++ test.py
@@ -1,20 +1,8 @@
-import time
-from multiprocessing import Process, Array, Lock
-import numpy as np
+import pickle
 
-def func(val, lock, startIndex):
-    dis = val.value
-    dis[startIndex][0] = 1
+path = "/Users/GongLi/Downloads/Histogram_YT_birthday_003.pkl"
+file = open(path, "r")
 
-if __name__ == '__main__':
+data = pickle.load(file)
 
-    distance = np.zeros((195, 195))
-
-    v = Array('d', distance)
-    lock = Lock()
-    procs = [Process(target=func, args=(v, lock, i)) for i in range(10)]
-
-    for p in procs: p.start()
-    for p in procs: p.join()
-
-    print v.value
+print "Yes"
